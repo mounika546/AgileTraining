5d584a434ff0628ce3e3c5bbc71df39e8c0317dd
mopeddir <mopeddir@DIN80012015.corp.capgemini.com>
2017-08-23 10:47:56 +0530

    message

---
+++ agile/src/main/java/com/agile/agile/MainClass.java
@@ -0,0 +1,12 @@
+package com.agile.agile;
+
+public class MainClass {
+	public static void main(String []args){
+		SalesClass salesClass=new SalesClass();
+		/*salesClass.add_method(num1, num2);*/
+		salesClass.print();
+		
+		
+	}
+
+}
