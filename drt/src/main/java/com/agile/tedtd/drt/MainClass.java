f489f162a7a81e1e07368e93f450e8ca2b34c245
mopeddir <mopeddir@DIN80012015.corp.capgemini.com>
2017-08-23 11:03:10 +0530

    added

---
+++ drt/src/main/java/com/agile/tedtd/drt/MainClass.java
@@ -0,0 +1,12 @@
+package com.agile.tedtd.drt;
+
+public class MainClass {
+	public static void main(String []args){
+		SalesClass salesClass=new SalesClass();
+		/*salesClass.add_method(num1, num2);*/
+		salesClass.print();
+		
+		
+	}
+
+}
---
+++ drt/src/main/java/com/agile/tedtd/drt/SalesClass.java
@@ -0,0 +1,23 @@
+package com.agile.tedtd.drt;
+
+public class SalesClass {
+/*	int[] arr={1,2,3,4};
+	for(int i=0;i<arr.size;i++)
+	{
+		System.out.println("arr size is.."+i);
+	}
+	*/
+	int i=0;
+	int j=1;
+	public int add_method(int num1,int num2)
+	{
+		int num3=num1+num2;
+		System.out.println("number is......."+num3);
+		return num3;
+		
+	}
+	public void print()
+	{
+		System.out.println("welcome to GIT..........");
+	}
+}
